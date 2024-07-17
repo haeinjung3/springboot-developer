e34172626d3c80cd175106f3bbda999348411939
haeinjung3 <haeinj1215@gmail.com>
2024-07-17 21:06:50 +0900

    chapter 6

--- src/main/java/me/junghaein/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/junghaein/springbootdeveloper/service/BlogService.java
@@ -1,8 +1,10 @@
 package me.junghaein.springbootdeveloper.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import me.junghaein.springbootdeveloper.domain.Article;
 import me.junghaein.springbootdeveloper.dto.AddArticleRequest;
+import me.junghaein.springbootdeveloper.dto.UpdateArticleRequest;
 import me.junghaein.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,24 @@ public class BlogService {
 
     public List<Article> findAll() {
         return blogRepository.findAll();
+    }
+
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id){
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
